e77dfb88eb86c1f286af8a4189969f3c9c6efa75
Leonardo Cerchioni <57871388+djtrexi@users.noreply.github.com>
2022-07-18 17:46:07 +0200

    Commit only client

--- ManagerCar/src/main/java/com/example/demo/model/Client.java
+++ ManagerCar/src/main/java/com/example/demo/model/Client.java
@@ -1,10 +1,14 @@
 package com.example.demo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Client {
@@ -19,6 +23,68 @@ public class Client {
 	private String email;
 	@Column(nullable = false, unique = true)
 	private String phone;
-	
+
+	@ManyToMany(mappedBy = "clients")
+	private List<Car> Cars = new ArrayList<>();
+
 	public Client() {}
+
+	public Client(long id, String name, String surname, String email, String phone) {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public Client(String name, String surname, String email, String phone) {
+		this.name = name;
+		this.surname = surname;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public List<Car> getCars() {
+		return Cars;
+	}
 }
