bafef5d3d3587b3113eddf714a1672e4a1961f0a
Leonardo Cerchioni <57871388+djtrexi@users.noreply.github.com>
2022-07-29 11:08:58 +0200

    add crossorigini commit

--- ManagerCar/src/main/java/com/example/demo/controller/CarController.java
+++ ManagerCar/src/main/java/com/example/demo/controller/CarController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -71,6 +72,7 @@ public class CarController {
 		}
 	}
 
+	@CrossOrigin(origins =  "*")
 	@RequestMapping(method = RequestMethod.POST, path = "/viewCarAvailable")
 	public ResponseEntity<CarPrintResponse> viewAllCarAvailable(){
 		List<Car> cars = serviceCar.viewAllCarAvailable();
