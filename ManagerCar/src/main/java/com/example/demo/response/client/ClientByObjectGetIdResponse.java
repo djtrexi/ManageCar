b8e6a85ff097a31cda5abd6eab9f62c802ff834a
Leonardo Cerchioni <57871388+djtrexi@users.noreply.github.com>
2022-11-10 18:43:13 +0100

    Commit for the client response back-end java

--- ManagerCar/src/main/java/com/example/demo/response/client/ClientByObjectGetIdResponse.java
+++ ManagerCar/src/main/java/com/example/demo/response/client/ClientByObjectGetIdResponse.java
@@ -2,18 +2,18 @@ package com.example.demo.response.client;
 
 import com.example.demo.model.Client;
 
-public class ClientSignResponse {
+public class ClientByObjectGetIdResponse {
 	private String name;
 	private String surname;
 
-	public ClientSignResponse() {}
-
-	public ClientSignResponse(Client c) {
+	public ClientByObjectGetIdResponse() {}
+	
+	public ClientByObjectGetIdResponse(Client c) {
 		name = c.getName();
 		surname = c.getSurname();
 	}
 
-	public ClientSignResponse(String name, String surname) {
+	public ClientByObjectGetIdResponse(String name, String surname) {
 		this.name = name;
 		this.surname = surname;
 	}
