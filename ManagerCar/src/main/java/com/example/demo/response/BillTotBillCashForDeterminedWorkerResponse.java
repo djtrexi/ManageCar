6607dc78c44cef0eed4091aa411ab69e22367c3e
Leonardo Cerchioni <57871388+djtrexi@users.noreply.github.com>
2022-11-10 18:40:38 +0100

    Commit for the create bill response back-end java

---
+++ ManagerCar/src/main/java/com/example/demo/response/BillTotBillCashForDeterminedWorkerResponse.java
@@ -0,0 +1,19 @@
+package com.example.demo.response.bill;
+
+public class BillTotBillCashForDeterminedWorkerResponse {
+	private int tot;
+
+	public BillTotBillCashForDeterminedWorkerResponse() {}
+
+	public BillTotBillCashForDeterminedWorkerResponse(int tot) {
+		this.tot = tot;
+	}
+
+	public int getTot() {
+		return tot;
+	}
+
+	public void setTot(int tot) {
+		this.tot = tot;
+	}
+}
